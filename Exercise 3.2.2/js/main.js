786827db552bfbfa24cb331afd87bf44dc8862e0
raquelmontilla <rmontillacalvo@gmail>
2019-09-03 13:39:39 +0200

    exercise 3.2.2

--- Exercise 3.2.2/js/main.js
+++ Exercise 3.2.2/js/main.js
@@ -9,33 +9,39 @@
 // [x] Crearemos tres instancias: una con 1 de lado, otra con 3 y otra con 7. Llamaremos a los dos métodos en todas las instancias.
 
 class Square {
-  constructor(lengthSides, base, height) {
+  constructor(lengthSides) {
     // Un atributo para la longitud del lado(side) del cuadrado
-    this.lengthSides1 = lengthSides1;
+    this.lengthSides = lengthSides;
   }
 
   // Un método para calcular el perímetro(perimeter()) del cuadrado(multiplica la longitud del lado por el número de lados - 4)
   perimeter() {
     // Los métodos no recibirán parámetros, sino que cogerán los datos necesarios de los atributos de la instancia.
-    const perimeter = this.lengthSides1 * this.lengthSides1;
+    const perimeter = this.lengthSides * 4;
     return perimeter;
 
   }
 
   // Un método para calcular el área(area()) del cuadrado(elevar al cuadrado el lado)
   area() {
-    const area = this.lengthSides1 * this.lengthSides1
+    const area = this.lengthSides * this.lengthSides
     return area;
 
   }
 }
-
-const polygon = new Square();
-polygon.perimeter(2);
-polygon.area(9);
 
 // Crearemos tres instancias: una con 1 de lado, otra con 3 y otra con 7. Llamaremos a los dos métodos en todas las instancias.
 
 const lengthSides1 = new Square(1);
 const lengthSides2 = new Square(3);
 const lengthSides3 = new Square(7);
+
+const polygon = new Square();
+// polygon.perimeter(2);
+// polygon.area(9);
+
+console.log(lengthSides1.perimeter())
+console.log(lengthSides2.perimeter())
+console.log(lengthSides3.perimeter())
+
+console.log(polygon)
